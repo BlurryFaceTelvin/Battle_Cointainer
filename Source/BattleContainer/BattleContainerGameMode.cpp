959b20e0d96f78df7fdc6fef3607547a0fc75e78
Telvin Njoroge <tnjoroge95@gmail.com>
2019-01-03 13:08:55 +0300

    Refactor by merging the NPC file with First person file to a new file called character

--- Source/BattleContainer/BattleContainerGameMode.cpp
+++ Source/BattleContainer/BattleContainerGameMode.cpp
@@ -10,7 +10,7 @@ ABattleContainerGameMode::ABattleContainerGameMode()
 {
 	// set default pawn class to our Blueprinted character
 	//static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Dynamic/Player/Behavior/FirstPersonCharacter"));
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Dynamic/NPC/Behavior/PatrollingGuard"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Dynamic/Character/Behavior/Character"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
