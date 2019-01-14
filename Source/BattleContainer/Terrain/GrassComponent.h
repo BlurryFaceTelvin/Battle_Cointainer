f8ac2feffcb9967021fe6f69c9c2aa59c263f2b1
Telvin Njoroge <tnjoroge95@gmail.com>
2019-01-14 11:58:14 +0300

    Procedural Spawn of grass into the environment

---
+++ Source/BattleContainer/Terrain/GrassComponent.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/HierarchicalInstancedStaticMeshComponent.h"
+#include "GrassComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLECONTAINER_API UGrassComponent : public UHierarchicalInstancedStaticMeshComponent
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
