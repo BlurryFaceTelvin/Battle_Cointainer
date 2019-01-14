ad40f0de0b385c117d320e5d37e480cee99d404b
Telvin Njoroge <tnjoroge95@gmail.com>
2019-01-14 11:59:14 +0300

    Spawn grass correctly

--- Source/BattleContainer/Terrain/GrassComponent.cpp
+++ Source/BattleContainer/Terrain/GrassComponent.cpp
@@ -2,6 +2,18 @@
 
 #include "GrassComponent.h"
 
+void UGrassComponent::BeginPlay()
+{
+	Super::BeginPlay();
+	SpawnGrass();
+}
 
-
-
+void UGrassComponent::SpawnGrass()
+{
+	//spawning the grass
+	for (size_t i = 0; i < SpawnCount; i++)
+	{
+		FVector Location = FMath::RandPointInBox(SpawningExtents);
+		AddInstance(FTransform(Location));
+	}
+}
--- Source/BattleContainer/Terrain/GrassComponent.h
+++ Source/BattleContainer/Terrain/GrassComponent.h
@@ -6,13 +6,25 @@
 #include "Components/HierarchicalInstancedStaticMeshComponent.h"
 #include "GrassComponent.generated.h"
 
-/**
- * 
- */
-UCLASS()
+
+UCLASS(ClassGroup = (Custom),meta=(BlueprintSpawnableComponent))
 class BATTLECONTAINER_API UGrassComponent : public UHierarchicalInstancedStaticMeshComponent
 {
 	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditDefaultsOnly, Category = Spawning)
+		FBox SpawningExtents;
+	UPROPERTY(EditDefaultsOnly, Category = Spawning)
+		int SpawnCount;
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+private:
+	void SpawnGrass();
+
+
+
 	
 	
 	
